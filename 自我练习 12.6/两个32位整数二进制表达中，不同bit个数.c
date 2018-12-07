55bd1373bd1d8d5aec3d4cd981e7eebc56f75432
Meatpowder <18209272770@163.com>
2018-12-07 18:30:34 +0800

    两个32位整数二进制表达中，不同bit个数.c

---
+++ 自我练习 12.6/两个32位整数二进制表达中，不同bit个数.c
@@ -0,0 +1,23 @@
+//int DeferentBits(int m, int n)
+//{
+//	int count = 0;
+//	while (m || n)
+//	{
+//		if ((m % 2) != (n % 2))
+//		{
+//			count++;
+//		}
+//		m /= 2;
+//		n /= 2;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int m = 1999;
+//	int n = 2299;
+//	printf("%d\n", DeferentBits(m, n));
+//	system("pause");
+//	return 0;
+//}
