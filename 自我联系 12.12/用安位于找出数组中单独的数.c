90210e60ee28fe7737b536df3d4206a6c2841559
Meatpowder <18209272770@163.com>
2018-12-12 23:05:26 +0800

    菜死了，还得练习

---
+++ 自我联系 12.12/用安位于找出数组中单独的数.c
@@ -0,0 +1,12 @@
+//int main()
+//{
+//	int arr[] = { 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7 };
+//	int i = 0;
+//	for (i = 1; i < sizeof(arr) / sizeof(arr[0]); i++)
+//	{
+//		arr[0] ^= arr[i];
+//	}
+//	printf("the num = %d\n", arr[0]);
+//	system("pause");
+//	return 0;
+//}
