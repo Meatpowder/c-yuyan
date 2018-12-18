ba3412ec66452902a34b3d7c07af90be8ec3206c
Meatpowder <18209272770@163.com>
2018-12-18 19:32:06 +0800

    tnedus ma i转化.c

---
+++ 自我练习 12.12（tnedus ma i)/ReverseBit.c
@@ -0,0 +1,19 @@
+void ReverseBit(unsigned int value)
+{
+	unsigned int sum = 0;
+	int i = 0;
+	for (; i < 32; i++)
+	{
+		sum += (((value >> i) & 1) << (31 - i));
+	}
+	printf("%u\n", sum);//此处一定要用%u而不是%d的，因为若最高位为1，sum代表的是有符号数，其结果为负数
+}
+
+int main()
+{
+	int value = 0;
+	scanf("%d", &value);
+	ReverseBit(value);
+	system("pause");
+	return 0;
+}
