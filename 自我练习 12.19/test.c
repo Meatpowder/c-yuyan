d270a3624414e0a734cf659baa5d585c0c7ace6b
Meatpowder <18209272770@163.com>
2018-12-19 22:36:01 +0800

    找出一个数二进制中的1.c

--- 自我练习 12.19/test.c
+++ 自我练习 12.19/test.c
@@ -82,3 +82,51 @@
 //	system("pause");
 //	return 0;
 //}
+
+//int main()
+//{
+//	int num = 10;
+//	int count = 0;
+//	while (num)
+//	{
+//		if ((num % 2) == 1)
+//		{
+//			count++;
+//		}
+//		num /= 2;
+//	}
+//	printf("%d\n", count);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int num = -10;
+//	int count = 0;
+//	int i = 0;
+//	for (; i < 32; i++)
+//	{
+//		if (((num >> i) & 1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d\n", count);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int num = -1;
+//	int count = 0;
+//	while (num)
+//	{
+//		count++;
+//		num = num & (num - 1);//只要进入就有不一样的位数
+//	}
+//	printf("%d\n", count);
+//	system("pause");
+//	return 0;
+//}
--- 自我练习 12.19/判断左右旋.c
+++ 自我练习 12.19/判断左右旋.c
@@ -1,48 +1,48 @@
-int main()
-{
-	char *p = "ABCDEFG";
-	int sz = strlen(p);
-	char arr1[100] = "";
-	int i = 0;
-	int j = 0;
-	for (; i < 2; i++)
-	{
-		char *p = "ABCDEFG";
-		for (j = 0; j < sz; j++)
-		{
-			arr1[j + i * sz] = *(p++);
-		}
-	}
-
-	char arr2[100] = "EFG";
-	int sz2 = strlen(arr2);
-
-	if (sz2 > sz)
-	{
-		printf("Enter erorr!\n");
-	}
-	else
-	{
-		j = 0;
-		for (i = 0; i < sz2; i++)
-		{
-			for (; j < (2 * sz); j++)
-			{
-				if (arr2[i] == arr1[j])
-				{
-					j++;
-					break;
-				}
-			}
-			if (i == (sz2 - 1) && j != (2 * sz))
-			{
-				printf("Correct!\n");
-				goto again;
-			}
-		}
-		printf("Erorr!\n");
-	}
-again:
-	system("pause");
-	return 0;
-}
+//int main()
+//{
+//	char *p = "ABCDEFG";
+//	int sz = strlen(p);
+//	char arr1[100] = "";
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 2; i++)
+//	{
+//		char *p = "ABCDEFG";
+//		for (j = 0; j < sz; j++)
+//		{
+//			arr1[j + i * sz] = *(p++);
+//		}
+//	}
+//
+//	char arr2[100] = "EFG";
+//	int sz2 = strlen(arr2);
+//
+//	if (sz2 > sz)
+//	{
+//		printf("Enter erorr!\n");
+//	}
+//	else
+//	{
+//		j = 0;
+//		for (i = 0; i < sz2; i++)
+//		{
+//			for (; j < (2 * sz); j++)
+//			{
+//				if (arr2[i] == arr1[j])
+//				{
+//					j++;
+//					break;
+//				}
+//			}
+//			if (i == (sz2 - 1) && j != (2 * sz))
+//			{
+//				printf("Correct!\n");
+//				goto again;
+//			}
+//		}
+//		printf("Erorr!\n");
+//	}
+//again:
+//	system("pause");
+//	return 0;
+//}
