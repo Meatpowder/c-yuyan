797f856ebc4604879ede1a74b2285f603d7e697f
Meatpowder <18209272770@163.com>
2018-12-22 23:21:47 +0800

    100到200素数、乘法口诀表、润年

---
+++ 第一次作业/100到200素数.c
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//#include<windows.h>
+//int main()
+//{int i = 100, n, k;
+//	{for (i; i <= 200; i++)
+//	{
+//		k = sqrt(i);
+//		for(n=2;n<=k;n++)
+//		{if(i%n==0) 
+//		break;
+//
+//	}
+//	if (n > k)
+//		printf("%d\t", i);
+//	}
+//
+//	}
+//	printf("\n");
+//	system ("pause");
+//	return 0;
+//}
---
+++ 第一次作业/判断1000到2000年是闰年.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int year, count;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (year % 4 == 0)
+//		{
+//			if (year % 100 != 0)
+//				printf("%d", year, count++);
+//		}
+//		if (year % 400 == 0)
+//			printf("%d", year, count++);
+//	}
+//	printf("\ncount=%d", count);
+//	system("pause");
+//	return 0;
+//}
