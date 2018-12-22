d02bb951c984f74df609138ab9632dc90c726cef
Meatpowder <18209272770@163.com>
2018-12-22 23:15:27 +0800

    折半查找.c

---
+++ 自我练习 10.17/折半查找.c
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	char arr[] = { 1,2,3,4,5,6,7,8,9 };
+//	int key = 0;
+//	int mid = 0;
+//	int left = 0;
+//	int right = sizeof(arr) / sizeof(arr[0]) - 1;
+//	printf("请输入1至9内的整数：");
+//	scanf("%d\n", &key);
+//	while (left <= right)
+//	{
+//		mid = (left + right) / 2;
+//		if (arr[mid] < key)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > key)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//		}
+//	if (left <= right)
+//	{
+//		printf("找到了，下标为：%d\n", mid);
+//	}
+//	else
+//	{
+//		printf("找不到");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//int bin_search(int key)
+//{
+//	char arr[] = { 1,2,3,4,5,6,7,8,9 };
+//	int mid = 0;
+//	int left = 0;
+//	int right = sizeof(arr) / sizeof(arr[0]) - 1;
+//	while (left <= right)
+//	{
+//		mid = (left + right) / 2;
+//		if (arr[mid] < key)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > key)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//	if (left <= right)
+//	{
+//		return mid;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+//int main()
+//{
+//	int key = 0;
+//	printf("请输入1至9内的整数：");
+//	scanf("%d\n", &key);
+//	int a = bin_search(key);
+//	printf("%d\n", a);
+//	system("pause");
+//	return 0;
+//}
+
+
+int main()
+{
+
+	return 0;
+}
