32c0ac4f805c5d3bdadaaeb52f4449e932c20b21
Meatpowder <18209272770@163.com>
2018-12-22 23:19:20 +0800

    循环语句.c

---
+++ 自我练习 10.12/循环语句.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS  
+//#include<stdio.h>
+//#include<windows.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	char arr1[] = "welcome to bit...";
+//	char arr2[] = "#################";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//	printf("%s\n", arr2);
+//	while (left <= right)
+//	{
+//		Sleep(1000);
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		left++;
+//		right--;
+//		printf("%s\n", arr2);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	char arr[10] = "";
+//	int i = 0;
+//	for (i = 0; i < 3; ++i)
+//	{
+//		printf("please input:");
+//		scanf("%s", arr);
+//		if (strcmp(arr, "250465") == 0)
+//			break;
+//		else
+//			printf("´íÎó");
+//	}
+//	if (i == 3)
+//		printf("ÍË³ö");
+//	else
+//		printf("½øÈë");
+//	system("pause");
+//	return 0;
+//}
+
+
+int mian()
+{
+	int i = 0;
+	int line = 10;
+	//scanf("%d", &line);
+	for (i = 0; i < line; i++)
+	{
+
+	}
+	return 0;
+}
